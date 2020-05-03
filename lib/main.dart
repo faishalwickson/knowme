5c1873e5b9c7081f09ce6d14a397dea1d1d5fa6a
wicak <faishalwicaksono9@gmail.com>
2020-05-03 17:14:04 +0700

    tes warna

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'KnowMe'),
     );
   }
 }
@@ -66,9 +66,12 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+
       appBar: AppBar(
+
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
+        backgroundColor: Colors.black,
         title: Text(widget.title),
       ),
       body: Center(
